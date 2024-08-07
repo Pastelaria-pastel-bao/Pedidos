7c89b4cad2bf4d46524893bbbd6a75cc183511e3
joaolenia <127161450+joaolenia@users.noreply.github.com>
2024-08-07 20:56:57 -0300

    Adicionando repository e exeptions

---
+++ Pedidos/src/main/java/com/pedidos/pedidos/repository/PedidosRepository.java
@@ -0,0 +1,11 @@
+package com.pedidos.pedidos.repository;
+
+
+import com.pedidos.pedidos.entity.Pedidos;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PedidosRepository extends JpaRepository<Pedidos, Long> {
+    Optional<Pedidos> findBySabor(String sabor);
+}
